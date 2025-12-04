8c2e8aeab7e7f48b20ceb8d7875e19ed9ad311bd
deAnkan <deyankan154@gmail.com>
2025-12-04 14:01:50 +0530

    notes.js got commented

--- notes.js
+++ notes.js
@@ -1,12 +1,12 @@
-console.log('Ankan is learning node_js');
+// console.log('Ankan is learning node_js');
 
-var age = 25;
-const addNumber = (a, b) => {
-    return a + b;
-}
+// var age = 25;
+// const addNumber = (a, b) => {
+//     return a + b;
+// }
 
 
-module.exports = {
-    age,
-    addNumber
-}; 
+// module.exports = {
+//     age,
+//     addNumber
+// }; 
